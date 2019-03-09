9a8bf0cef57a21615c71dfcbd1a696af41b2a842
Mārtiņš Liberts <martins.liberts@gmail.com>
2019-03-09 11:32:46 +0200

    SDDF update according to the ESS Data Protocol.

--- code/62-sample-format.R
+++ code/62-sample-format.R
@@ -8,9 +8,6 @@ options(stringsAsFactors = F)
 # Packages ####
 require(data.table)
 require(openxlsx)
-require(ggplot2)
-require(OpenStreetMap)
-require(sampling)
 
 
 # Reset ####
@@ -49,7 +46,8 @@ degurba_luz <- merge(x = degurba, y = luz, by = "nov_code", all = T)
 degurba_luz[is.na(frame2), frame2 := "LV000"]
 degurba_luz
 
-dcast.data.table(data = degurba_luz, formula = frame2 ~ frame1)
+dcast.data.table(data = degurba_luz, formula = frame2 ~ frame1,
+                 fun.aggregate = length, value.var = "nov_code")
 
 # Sample ####
 
@@ -61,12 +59,8 @@ sample_majo <- merge(x = sample_majo, y = degurba_luz, by = "nov_code",
 
 sample_majo[, .N, keyby = .(frame1)]
 sample_majo[, .N, keyby = .(frame2)]
-dcast.data.table(data = sample_majo, formula = frame2 ~ frame1)
-
-
-# Extra variables
-sample_majo[, prob3 := NA]
-sample_majo[, outcome := NA]
+dcast.data.table(data = sample_majo, formula = frame2 ~ frame1,
+                 fun.aggregate = length, value.var = "idno")
 
 
 # Number of enterprises and organisation registered at the dwelling address
@@ -78,31 +72,88 @@ sample_majo[, as.list(summary(frame3)), keyby = .(stratex1)]
 sample_majo[, frame4 := psu_pop]
 
 
+# Recode variables according to the SDDF specifiaction ####
+
+ESS9_LV_STRATEX1 <- unique(sample_majo[, .(stratex1)])
+setorder(ESS9_LV_STRATEX1)
+ESS9_LV_STRATEX1[, STRATEX1 := 1:.N]
+ESS9_LV_STRATEX1
+
+ESS9_LV_STRTVAL1 <- unique(sample_majo[, .(strtval1)])
+setorder(ESS9_LV_STRTVAL1)
+ESS9_LV_STRTVAL1[, STRTVAL1 := 1:.N]
+ESS9_LV_STRTVAL1
+
+ESS9_LV_STRTVAL2 <- unique(sample_majo[, .(strtval2)])
+setorder(ESS9_LV_STRTVAL2)
+ESS9_LV_STRTVAL2[, STRTVAL2 := 1:.N]
+ESS9_LV_STRTVAL2
+
+ESS9_LV_PSU <- unique(sample_majo[, .(stratex1, stratim1, psu)])
+setorder(ESS9_LV_PSU)
+ESS9_LV_PSU[, PSU := 1:.N]
+ESS9_LV_PSU[, STRATIM1 := 1:.N]
+ESS9_LV_PSU
+
+ESS9_LV_SSU <- unique(sample_majo[, .(stratex1, stratim1, psu, stratim2, idno)])
+setorder(ESS9_LV_SSU)
+ESS9_LV_SSU[, SSU := 1:.N]
+ESS9_LV_SSU[, STRATIM2 := 1:.N]
+ESS9_LV_SSU[, all.equal(idno, SSU)]
+ESS9_LV_SSU
+
+sample_majo_2 <- copy(sample_majo)
+sample_majo_2 <- merge(sample_majo_2, ESS9_LV_STRATEX1, by = "stratex1")
+sample_majo_2 <- merge(sample_majo_2, ESS9_LV_STRTVAL1, by = "strtval1")
+sample_majo_2 <- merge(sample_majo_2, ESS9_LV_STRTVAL2, by = "strtval2")
+sample_majo_2 <- merge(sample_majo_2, ESS9_LV_PSU[, .(psu, PSU, STRATIM1)], by = "psu")
+sample_majo_2 <- merge(sample_majo_2, ESS9_LV_SSU[, .(idno, SSU, STRATIM2)], by = "idno")
+setorder(sample_majo_2, idno)
+
+
 # Sample file for the fieldwork ####
+ESS9_LV_sample <- sample_majo_2[, .(IDNO = idno,
+                                    STRATEX1, STRATIM1, STRATIM2,
+                                    PSU, SSU,
+                                    reg_code, reg_name,
+                                    nov_code, nov_name,
+                                    ATVK_code, atvk_name,
+                                    pil_lauk, pil_lauk_name,
+                                    apkaime_gid, apkaime,
+                                    adr_kods, adr_kods_eka, adr_kods_dziv,
+                                    adrese,
+                                    koord_x, koord_y, lat, lon)]
 
-ESS9_LV_sample <- sample_majo[, .(idno, stratex1, stratim1, stratim2,
-                                  reg_code, reg_name,
-                                  nov_code, nov_name,
-                                  ATVK_code, atvk_name,
-                                  pil_lauk, pil_lauk_name,
-                                  apkaime_gid, apkaime,
-                                  psu,
-                                  adr_kods, adr_kods_eka, adr_kods_dziv,
-                                  adrese,
-                                  koord_x, koord_y, lat, lon)]
 
-ESS9_LV_SDDF <- sample_majo[, .(idno,
-                                prob1, prob2, prob3,
-                                stratex1,
-                                stratim1, stratim2,
-                                strtval1, strtval2,
-                                psu, outcome,
-                                frame1, frame2, frame3, frame4)]
+# SDDF ####
+ESS9_LV_SDDF <- sample_majo_2[, .(idno,
+                                  CNTRY = "LV",
+                                  prob1,
+                                  prob2,
+                                  prob3 = NA,
+                                  prob4 = NA,
+                                  PSU,
+                                  SAMPPOIN = NA,
+                                  SSU,
+                                  STRATEX1,
+                                  STRATIM1,
+                                  STRATIM2,
+                                  STRTVAL1,
+                                  STRTVAL2,
+                                  STRTVAL3 = NA,
+                                  OUTCOME = NA,
+                                  frame1, frame2, frame3, frame4)]
+setnames(ESS9_LV_SDDF, toupper(names(ESS9_LV_SDDF)))
+ESS9_LV_SDDF
+
+
+# Test ####
+
+ESS9_LV_SDDF[, sum(1 / (PROB1 * PROB2))]
 
 
 # Save ####
 
-sample_majo_2 <- sample_majo
 save(sample_majo_2, file = "results/sample_majo_2.Rdata")
 
 fwrite(x = ESS9_LV_sample, file = "results/sent/ESS9_LV_sample.csv")
@@ -110,3 +161,6 @@ fwrite(x = ESS9_LV_SDDF, file = "results/sent/ESS9_LV_SDDF.csv")
 
 write.xlsx(x = ESS9_LV_sample, file = "results/sent/ESS9_LV_sample.xlsx")
 write.xlsx(x = ESS9_LV_SDDF, file = "results/sent/ESS9_LV_SDDF.xlsx")
+write.xlsx(x = list(ESS9_LV_STRATEX1, ESS9_LV_STRTVAL1, ESS9_LV_STRTVAL2,
+                    ESS9_LV_PSU, ESS9_LV_SSU),
+           file = "results/sent/ESS9_LV_variable_recode.xlsx")
